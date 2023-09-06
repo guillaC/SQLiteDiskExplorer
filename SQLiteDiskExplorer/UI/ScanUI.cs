1c657c5e7ded301c719215a82bd79aa1cf780b8a
Gui <6315083+guillaC@users.noreply.github.com>
2023-09-06 20:08:18 +0200

    WIP ScanUI

--- SQLiteDiskExplorer/UI/ScanUI.cs
+++ SQLiteDiskExplorer/UI/ScanUI.cs
@@ -1,21 +1,35 @@
 ﻿using ImGuiNET;
 using SQLiteDiskExplorer.Core;
+using SQLiteDiskExplorer.Model;
 using SQLiteDiskExplorer.Utils;
+using System.Data.Common;
 using System.Numerics;
 
 namespace SQLiteDiskExplorer.UI
 {
     public class ScanUI
     {
-        bool firstLoad = true; 
+        bool firstLoad = true;
         bool isOpen = true;
         bool cancelProcessing = false;
 
         Dictionary<DriveInfo, List<FileInfo>> DrivePathsMap = new();
         Dictionary<DriveInfo, SQliteScan> Workers = new();
 
+        AppConfig config;
+
         public ScanUI(List<DriveInfo> pSelectedDrive)
         {
+            config = ConfigurationManager.LoadConfiguration();
+            config.ImportantKeywords = config.ImportantKeywords.ConvertAll(d => d.ToLower());
+
+            Console.WriteLine("la config est chargée et contient " + config.ImportantKeywords.Count);
+
+            foreach (string ze in config.ImportantKeywords)
+            {
+                Console.WriteLine(ze);
+            }
+
             foreach (var drive in pSelectedDrive)
             {
                 DrivePathsMap.Add(drive, new List<FileInfo>());
@@ -43,7 +57,7 @@ namespace SQLiteDiskExplorer.UI
 
         private void ApplyStyle()
         {
-            // ImGui.PushStyleColor(ImGuiCol.PlotHistogram, new Vector4(11, 22, 33, 44));
+            ImGui.PushStyleColor(ImGuiCol.PlotHistogram, (Vector4)Color.Violet);
         }
 
         private void ShowProgress()
@@ -110,12 +124,18 @@ namespace SQLiteDiskExplorer.UI
                         {
                             ImGui.BeginTable("Files", 3, ImGuiTableFlags.Resizable | ImGuiTableFlags.NoSavedSettings | ImGuiTableFlags.Borders);
                             ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.NoResize, 0.78f);
-                            ImGui.TableSetupColumn("Date",ImGuiTableColumnFlags.NoResize, 0.15f);
-                            ImGui.TableSetupColumn("Size",ImGuiTableColumnFlags.NoResize,0.07f);
+                            ImGui.TableSetupColumn("Date", ImGuiTableColumnFlags.NoResize, 0.15f);
+                            ImGui.TableSetupColumn("Size", ImGuiTableColumnFlags.NoResize, 0.07f);
                             ImGui.TableHeadersRow();
-                            foreach(FileInfo file in info.Value)
+                            foreach (FileInfo file in info.Value)
                             {
                                 ImGui.TableNextColumn();
+                                if (config.CheckPathKeywordPresence && config.ImportantKeywords.Any(keyword => file.FullName.Contains(keyword.ToLower())))
+                                {
+                                    ImGui.TextColored((Vector4)Color.Violet, "[KeywordInPath]");
+                                    ImGui.SameLine();
+                                }
+
                                 ImGui.Text(file.FullName);
                                 ImGui.TableNextColumn();
                                 ImGui.Text(file.CreationTime.ToString());
