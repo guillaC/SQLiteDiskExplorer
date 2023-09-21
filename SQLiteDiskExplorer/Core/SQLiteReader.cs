c351c01662a42beacb90059d006398f95a231788
Gui <6315083+guillaC@users.noreply.github.com>
2023-09-21 23:06:50 +0200

    WIP

--- SQLiteDiskExplorer/Core/SQLiteReader.cs
+++ SQLiteDiskExplorer/Core/SQLiteReader.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,39 @@ namespace SQLiteDiskExplorer.Core
 {
     public class SQLiteReader
     {
+        private readonly SqliteConnection Connection;
+        public Dictionary<string, string> TableStructure = new(); // à remplacer par un objet TableStruc
+
+        public SQLiteReader(SqliteConnection file)
+        {
+            Connection = new SqliteConnection($"Data Source={file};Version=3;");
+            LoadTableStructure();
+        }
+
+        private void LoadTableStructure()
+        {
+            Connection.Open();
+            DataTable table = Connection.GetSchema("Tables");
+
+            foreach (DataRow row in table.Rows)
+            {
+                string tableName = row["TABLE_NAME"]?.ToString() ?? "";
+                if (!string.IsNullOrWhiteSpace(tableName))
+                {
+                    DataTable schemaTable = Connection.GetSchema("Columns", new[] { null, null, tableName });
+                    List<string> columns = new();
+
+                    foreach (DataRow schemaRow in schemaTable.Rows)
+                    {
+                        string columnName = schemaRow["COLUMN_NAME"]?.ToString()??"";
+                        if (!string.IsNullOrWhiteSpace(columnName)) columns.Add(columnName);
+                    }
+
+                    TableStructure[tableName] = string.Join(", ", columns); // TODO : pas tenable
+                }
+            }
+
+            Connection.Close();
+        }
     }
 }
